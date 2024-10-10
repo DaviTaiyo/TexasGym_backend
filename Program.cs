abd1a2138c21aabbc4c7451e1c8fa936d394c33f
Davi Paulino <78768199+DaviTaiyo@users.noreply.github.com>
2024-10-09 21:54:14 -0300

    TESTE NA FAFIBE

--- Program.cs
+++ Program.cs
@@ -1,23 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using texasgym_backend.Data; // Certifique-se de que este namespace está correto
+using texasgym_backend.Data;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Configuração do Entity Framework com o banco de dados SQLite
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
